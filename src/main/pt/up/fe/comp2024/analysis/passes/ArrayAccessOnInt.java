e330014c39e6713f1d17fd5f5c309db20e944aa8
João Sousa <up202106996@up.pt>
2024-04-20 00:46:31 +0100

    revert to last commit

--- src/main/pt/up/fe/comp2024/analysis/passes/ArrayAccessOnInt.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/ArrayAccessOnInt.java
@@ -38,8 +38,6 @@ public class ArrayAccessOnInt extends AnalysisVisitor{
                 return null;
             if(Objects.equals(arrayAccess.getParent().getKind(), "BinaryOp"))
                 return null;
-            if(Objects.equals(arrayAccess.getChild(0).getKind(), "ArrayInit"))
-                return null;
             String assignedTo = arrayAccess.getParent().get("var");
             String assignedToType = null;
             for(JmmNode varDecl : methodDecl.getChildren(Kind.VAR_DECL)){
